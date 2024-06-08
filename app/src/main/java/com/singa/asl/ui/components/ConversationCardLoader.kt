7ef38abc0b4b8766d5627107c429299a05c1d968
rizrmdhn <114452320+rizrmdhn@users.noreply.github.com>
2024-06-08 09:00:53 +0800

    chore: change conversation card loader width to fixed

--- app/src/main/java/com/singa/asl/ui/components/ConversationCardLoader.kt
+++ app/src/main/java/com/singa/asl/ui/components/ConversationCardLoader.kt
@@ -23,7 +23,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import kotlin.random.Random
 
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
